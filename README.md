a8efaaa6476651fa01c617f35a810c1719c6d09a
徐鸿超 <114685706+xuhongchaoVSgaoyuanhui@users.noreply.github.com>
2024-09-15 12:33:06 +0800

    初始commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# MyDemo
+
+Developed with Unreal Engine 4
---
+++ Source/MyDemo/MyDemoGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MyDemoGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MYDEMO_API AMyDemoGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
